861375a257dfe9278d81ad6c8c6bb965d0e05ce4
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-07-17 21:06:04 -0700

    App and root components created and added to entry file

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,39 @@
+
+import * as APIUtil from '../util/session_api_util';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+
+// actions are creating JSON objects that will go to a reducer and
+// make a change to the state and post it to the store, the reason
+// that we have thunk action creators
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+// Thunk action creators need to make an AJAX call before it can go to
+// the reducer so when thunk our middle ware sees we are passing a function
+// (which is an ajax request) it will perform that function and then dispatch
+// a regular action creator that will pass a regular object to the reducer
+export const login = (user) => dispatch => (
+  APIUtil.login(user).then(user => dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  )
+);
+
+export const logout = () => dispatch => (
+  APIUtil.logout().then((user) => dispatch(receiveCurrentUser(null))
+));
+
+export const signup = (user) => dispatch => (
+  APIUtil.signup(user).then(user => dispatch(receiveCurrentUser(user))),
+  err => ( dispatch(receiveErrors(err.responseJSON)))
+);
---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>DRIFT MUSIC</h1>
+  </div>
+);
+
+export default App;
