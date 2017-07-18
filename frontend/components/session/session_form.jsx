9ba971efeced0c9b9ae005074ff953457dbe2ccb
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-07-18 10:45:03 -0700

    User login/logout/signup is functional, and rendering this is a basic skeleton of a rails project with some user authentication

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
@@ -6,38 +7,27 @@ class SessionForm extends React.Component {
     super(props);
     this.state = {
       username: '',
-      password: '',
+      password: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // if logged in then add the '/' homepage
-  // to the history
   componentWillReceiveProps(nextProps) {
     if (nextProps.loggedIn) {
       this.props.history.push('/');
     }
   }
 
-  // passing a field (a key in the state)
-  // and updating it to the current value
-  // of what is in the input
   update(field) {
     return e => this.setState({
       [field]: e.currentTarget.value
     });
   }
 
-  // takes the information in the currentState
-  // that is being updated by update and when submitted
-  // will save that state in a new object and pass that
-  // to the processForm function that will either create
-  // a new user or log in the user depending on the
-  // form type
   handleSubmit(e) {
     e.preventDefault();
-    const user = Object.assign({}, this.state);
-    this.props.processForm(user);
+    const user = this.state;
+    this.props.processForm({user});
   }
 
   navLink() {
@@ -64,7 +54,7 @@ class SessionForm extends React.Component {
     return (
       <div className="login-form-container">
         <form onSubmit={this.handleSubmit} className="login-form-box">
-          Welcome to DRIFT!
+          Welcome to BenchBnB!
           <br/>
           Please {this.props.formType} or {this.navLink()}
           {this.renderErrors()}
--- frontend/components/session/session_form_container.js
+++ frontend/components/session/session_form_container.js
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
-
+import { login, logout, signup } from '../../actions/session_actions';
 import SessionForm from './session_form';
+
+
 
 const mapStateToProps = ({ session }) => {
   return {
