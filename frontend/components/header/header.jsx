c0b9c3d4fe3406222a1d42fbb184e390545609f4
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-07-23 22:42:54 -0700

    some css touches final push for sunday

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -20,19 +20,28 @@ class Header extends React.Component {
     }
   }
 
+  currentUser() {
+    if (this.props.currentUser) {
+      return <h1>{currentUser.name}</h1>
+    }
+    return <h1>Guest login</h1>
+  }
+
   handleClick() {
     this.props.logout().then(() => this.props.history.push("/"));
   }
 
   render() {
+    console.log(this.props)
     return (
 
       <div className="header-render">
         <div className="app-title-icon-combo">
           <img src="http://res.cloudinary.com/jlofton/image/upload/v1500496705/message-in-a-bottle-white_hc6fkx.svg" className="icon"/>
-          <Link to="/"><h1 className="App-title">DRIFT</h1></Link>
+          <Link to="/music/artists"><h1 className="App-title">DRIFT</h1></Link>
         </div>
         <div className="header-nav-bar">
+          <div className="header-current-user"></div>
           <div className="loginout">{ this.navLink() }</div>
         </div>
       </div>
