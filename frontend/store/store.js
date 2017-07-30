f63dcce2c5af8b5ad3b266938ad8d9f5fa910359
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-07-30 14:29:35 -0700

    removed console logs and debugger statements and removed minification warnings, and only using development tools in dev environment

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
 import RootReducer from '../reducers/root_reducer';
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const createLogger = require('redux-logger');
+  middlewares.push(createLogger());
+}
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
