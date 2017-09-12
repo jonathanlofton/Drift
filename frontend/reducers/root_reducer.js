b424cf091e87fa9359b705e282af4c12af944252
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-11 21:53:25 -0700

    Deleted unused player reducer and state

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import ArtistReducer from './artist_reducer';
 import SongReducer from './song_reducer';
-import AudioPlayerReducer from './audio_player_reducer';
 import AlbumReducer from './album_reducer';
 import PlaylistReducer from './playlist_reducer';
 
@@ -10,7 +9,6 @@ const RootReducer = combineReducers({
   session: SessionReducer,
   artists: ArtistReducer,
   songs: SongReducer,
-  player: AudioPlayerReducer,
   albums: AlbumReducer,
   playlist: PlaylistReducer,
 });
