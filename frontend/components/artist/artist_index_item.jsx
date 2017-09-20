d83e16ec288067297eff862561319e07fd4a1cbe
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-20 13:44:09 -0700

    removed unused code in artist index item component

--- frontend/components/artist/artist_index_item.jsx
+++ frontend/components/artist/artist_index_item.jsx
@@ -2,14 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class ArtistIndexItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-
-  }
 
   render() {
     const { artist } = this.props;
