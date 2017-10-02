902f45e0b7c61ffd4fc06f3ec04ccfe67c58dc54
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-10-01 18:59:13 -0700

    removed actions from the window

--- frontend/spotify_clone.jsx
+++ frontend/spotify_clone.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { fetchArtists, fetchArtist } from './actions/artist_actions';
-import { fetchSongs } from './actions/song_actions';
-import { fetchAlbums } from './actions/album_actions';
-import { fetchPlaylists, addPlaylistToList } from './actions/playlist_actions';
-import { fetchPlaylistSongs } from './actions/playlist_song_actions';
 import configureStore from './store/store';
 import Root from './components/root';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
-  window.fetchPlaylists = fetchPlaylists;
-  window.fetchSongs = fetchSongs;
-  window.fetchArtist = fetchArtist;
-  window.fetchArtists = fetchArtists;
-  window.fetchAlbums = fetchAlbums;
-  window.fetchPlaylistSongs = fetchPlaylistSongs;
-  window.addPlaylistToList = addPlaylistToList;
+
   if (window.currentUser) {
     const preloadedState = { session: { currentUser: window.currentUser }};
     store = configureStore(preloadedState);
@@ -25,9 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
